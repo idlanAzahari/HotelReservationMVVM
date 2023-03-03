5843258dd0dd7633ea38adfc1f89795f2a7088c4
Idlan Azahari <idlan.azahari@cadcam.com.my>
2023-03-03 15:37:56 +0800

    adding views

--- HotelReservation/App.xaml.cs
+++ HotelReservation/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelReservation.Exceptions;
+using HotelReservation.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,32 @@ namespace HotelReservation
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Modulus INN");
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                new RoomID(1, 20), new DateTime(2020, 10, 20), new DateTime(2020, 10, 25), "Idlan Azahari"
+                ));
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 20), new DateTime(2020, 10, 20), new DateTime(2020, 10, 28), "John Smith"
+                    ));
+            }
+            catch (ReservationConflictExceptions ex)
+            {
+
+                
+            }
+            
+
+            
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("Idlan Azahari"); //hmmm menarikk
+
+
+            base.OnStartup(e);
+        }
     }
 }
--- HotelReservation/MVVM/Models/RoomID.cs
+++ HotelReservation/MVVM/Models/RoomID.cs
@@ -35,5 +35,20 @@ namespace HotelReservation.MVVM.Models
         {
             return HashCode.Combine(FloorNumber, RoomNumber);    // interesting boleh baca nanti
         }
+
+        public static bool operator == (RoomID roomID1, RoomID roomID2)
+        {
+            if (roomID1 is null && roomID2 is null)
+            {
+                return true;
+            }
+
+            return !(roomID1 is null) && roomID1.Equals(roomID2);
+        }
+
+        public static bool operator != (RoomID roomID1, RoomID roomID2)
+        {
+            return !(roomID1 == roomID2);
+        }
     }
 }

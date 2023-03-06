ff889ab4124edd4fd0923c2806856374077a0a4c
Idlan Azahari <idlan.azahari@cadcam.com.my>
2023-03-06 17:50:31 +0800

    Add properties to MakeReservationVM

---
+++ HotelReservation/MVVM/View Model/BaseVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.MVVM.View_Model
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+
+    }
+}
---
+++ HotelReservation/MVVM/View Model/MakeReservationVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.MVVM.View_Model
+{
+    public class MakeReservationVM : BaseVM
+    {
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; OnPropertyChanged(nameof(UserName)); }
+        }
+
+        private int _floorNumber;
+
+        public int FloorNumber
+        {
+            get { return _floorNumber; }
+            set { _floorNumber = value; OnPropertyChanged(nameof(FloorNumber)); }
+        }
+
+        private int _roomNumber;
+
+        public int RoomNumber
+        {
+            get { return _roomNumber; }
+            set { _roomNumber = value; OnPropertyChanged(nameof(RoomNumber)); }
+        }
+
+        private DateTime _startTime;
+
+        public DateTime StartTime
+        {
+            get { return _startTime; }
+            set { _startTime = value; OnPropertyChanged(nameof(RoomNumber)); }
+        }
+
+
+        private DateTime _endDate;
+
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; OnPropertyChanged(nameof(EndDate)); }
+        }
+
+
+
+    }
+}
